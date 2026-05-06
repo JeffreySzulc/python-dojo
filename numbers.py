d7689db142fd4620003ad7735d67d1c55b76df3f
JeffreySzulc <szulcjeffreyj@gmail.com>
2026-05-06 12:51:56 -0400

    practice with numbers python script

---
+++ numbers.py
@@ -0,0 +1,11 @@
+num1 = int(input("Give me a number: "))
+num2 = int(input("Give me a number: "))
+num3 = int(input("Give me a number: "))
+num4 = int(input("Give me a number: "))
+num5 = int(input("Give me a number: "))
+
+total = (num1 + num2 + num3 + num4 + num5)
+average = (total/5)
+
+print("total is: ", total)
+print("average is: ", average)

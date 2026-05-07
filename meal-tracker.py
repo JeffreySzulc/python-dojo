23aca06ca480c54100504721dfe3b59ddf0c6478
JeffreySzulc <szulcjeffreyj@gmail.com>
2026-05-07 15:20:23 -0400

    protein meal tracker

---
+++ meal-tracker.py
@@ -0,0 +1,13 @@
+food1 = input("Name the food, Dawg: ")
+protein1 = int(input("How much protein does it have?: "))
+
+food2 = input("Name the food, Dawg: ")
+protein2 = int(input("How much protein does it have?: "))
+
+food3 = input("Name the food, Dawg: ")
+protein3 = int(input("How much protein does it have?: "))
+
+total = protein1 + protein2 + protein3
+
+print(f"Todays meal is: \n{food1}\n{food2}\n{food3}")
+print(f"Total protein = {total}")

ba6102ba56d8d519572ba54dc628ef93ef8b08ad
JeffreySzulc <szulcjeffreyj@gmail.com>
2026-05-08 13:56:31 -0400

    upgrade to meal tracker

--- meal-tracker.py
+++ meal-tracker.py
@@ -1,3 +1,5 @@
+# Meal 1
+
 food1 = input("Name the food, Dawg: ")
 protein1 = int(input("How much protein does it have?: "))
 
@@ -7,7 +9,30 @@ protein2 = int(input("How much protein does it have?: "))
 food3 = input("Name the food, Dawg: ")
 protein3 = int(input("How much protein does it have?: "))
 
-total = protein1 + protein2 + protein3
+meal1 = f"{food1}, {food2}, and {food3}"
+total1 = protein1 + protein2 + protein3
 
-print(f"Todays meal is: \n{food1}\n{food2}\n{food3}")
-print(f"Total protein = {total}")
+# Meal 2
+
+food4 = input("Name the food, Dawg: ")
+protein4 = int(input("How much protein does it have?: "))
+
+food5 = input("Name the food, Dawg: ")
+protein5 = int(input("How much protein does it have?: "))
+
+food6 = input("Name the food, Dawg: ")
+protein6 = int(input("How much protein does it have?: "))
+
+meal2 = f"{food4}, {food5}, and {food6}"
+total2 = protein4 + protein5 + protein6
+
+
+if(total1 > total2):
+    print(f"Meal 1 {meal1} has more protein.")
+elif(total2 > total1):  
+    print(f"Meal 2 {meal2} has more protein.")
+else:
+    print("Meal 1 and Meal 2 have the same exact amount of protein, would ya believe it??")
+# print(f"Todays meal is: \n{food1}\n{food2}\n{food3}")
+# print(f"Total protein = {total}")
+# print(meal1)
